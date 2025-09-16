a51bd0536e586ff6dd1acb394abfc64020c911e6
Jort de Vreeze <mail@jortdevreeze.com>
2025-09-16 10:18:45 +0200

    [FIX] Update version number to reflect latest changes in partner birthday module

--- partner_birthday/__manifest__.py
+++ partner_birthday/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Partner Birthday',
-    'version': '1.0.0',
+    'version': '18.0.1.0.0',
     'summary': 'Add date of birth to partners and set up birthday reminders',
     'description': '''
         This module adds a date of birth field to partners and automatically
